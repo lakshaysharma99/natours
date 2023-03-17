3c070d79ddbca86e9532b1dc7c6492d36cd285bb
lakshaysharma99 <lakshays206@gmail.com>
2023-03-17 09:19:28 +0530

    added tour routes; from pc

---
+++ app.js
@@ -0,0 +1,98 @@
+const fs = require('fs')
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+)
+
+const getAllTours = (req,res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+}
+
+const getTour = (req,res) => {
+    console.log(req.params)
+    const id = req.params.id * 1
+
+    if(id> tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+
+    const tour = tours.find(el => el.id === id)
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
+}
+
+
+
+app.get('/api/vq/tours', getAllTours)
+
+// optional parameter: add ? after a paran that you want to be optional
+// '/api/v1/tours/:id:/x?'
+app.get('/api/v1/tours/:id', getTour)
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body)
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id:newId}, req.body)
+    
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
+    })
+})
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+    if(req.params.id * 1> tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Updated tour here...>'
+        }
+    })
+})
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+    if(req.params.id * 1> tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+})
+
+app.listen(5000, () => {
+    console.log('app running on port 5000')
+})
